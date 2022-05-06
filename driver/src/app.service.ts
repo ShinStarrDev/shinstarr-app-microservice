b575cb0f70a6ac8fb1cdda8a86ff204672fee447
sik <jsjsjskjs@gmail.com>
2022-05-06 18:03:34 +0900

    added hostname

--- driver/src/app.service.ts
+++ driver/src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import * as os from 'os';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! driver server';
+    return `Hello World! main server - host: ${os.hostname}`;
   }
 }
--- main/src/app.service.ts
+++ main/src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import * as os from 'os';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! main server';
+    return `Hello World! main server - host: ${os.hostname}`;
   }
 }
